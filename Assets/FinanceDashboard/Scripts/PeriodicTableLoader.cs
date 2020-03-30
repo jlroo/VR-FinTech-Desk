984042c4fb7924156cebb5ad11d919786236466f
Elizabeth Wang <eywang1999@gmail.com>
2020-03-30 11:22:38 -0700

    Integrated parsed subojbjects with CompanyData and fixed display bug

--- Assets/FinanceDashboard/Scripts/PeriodicTableLoader.cs
+++ Assets/FinanceDashboard/Scripts/PeriodicTableLoader.cs
@@ -5,75 +5,179 @@
 using Microsoft.MixedReality.Toolkit.Utilities;
 using System.Collections.Generic;
 using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+//using System.Text.Json;
+//using System.Text.Json.Serialization;
+using System;
+
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace HoloToolkit.MRDL.PeriodicTable
 {
 
+    ////////// FOR PARSING COMPANIES.JSON /////////
     [System.Serializable] 
-    class CompanyName { 
-      public string name;
+    public class CompanyName { 
+      public string stock_name;
+      public string news_name;
     }
 
     [System.Serializable]
-    class CompanyNames {
+    public class CompanyNames {
         public List<CompanyName> names;
 
         public static CompanyNames FromJSON(string json) {
             return JsonUtility.FromJson<CompanyNames>(json);
+            // JsonSerializer.Deserialize<WeatherForecast>(jsonString);
         }
     }
 
+    ////////// FOR PARSING FROM THE NEWS API /////////
     [System.Serializable]
-    class CompanyNews {
+    public class CompanyNews {
       public string title;
       public string url;
-      // TODO not sure if we want to store anything else 
-    }
-
-    // All news pertaining to a company
-    [System.Serializable]
-    class AllNews {
-      public List<CompanyNews> allNews;
-
-      public static AllNews FromJSON(string json) {
-          return JsonUtility.FromJson<AllNews>(json);
+      // TODO is there anything else we want to store?
+      
+      public string toString() {
+        StringBuilder dataBuilder = new StringBuilder();
+        dataBuilder.Append("Title: ");
+        dataBuilder.AppendLine(title);
+        dataBuilder.Append("Url: ");
+        dataBuilder.AppendLine(url);
+        return dataBuilder.ToString();
       }
     }
-  
-    // <> Adjusted Time Series
-    [System.Serializable]
-    class CompanyStock {
-      public string date;
 
-      /* Example: 
-            "1. open": "94.6000",
-            "2. high": "113.1500",
-            "3. low": "90.5600",
-            "4. close": "108.0300",
-            "5. adjusted close": "108.0300",
-            "6. volume": "39907532",
-            "7. dividend amount": "0.0000" 
-      */
-      public string raw_data;
-      // TODO not storing metadata?
+    // Top N news articles for a single a company
+    [System.Serializable]
+    public class AllNews {
+      public List<CompanyNews> articles;
+      public static int NUM_NEWS_ARTICLES = 3; // Number of news articles to store 
+
+      public static AllNews FromJSON(string json) {
+        AllNews allNews = JsonUtility.FromJson<AllNews>(json);
+        // return JsonSerializer.Deserialize<AllNews>(json);
+        // TODO allNews.articles.Take(num_articles);
+        return allNews;
+      }
+
+      public string toString() { // Debugging
+        StringBuilder dataBuilder = new StringBuilder();
+        foreach (CompanyNews article in articles) {
+          dataBuilder.AppendLine(article.toString());
+        }
+        return dataBuilder.ToString(); 
+      }
+    }
+ 
+    ////////// FOR PARSING FROM THE NEWS API /////////
+    // All stock prices for a single company
+    [System.Serializable]
+    public class MetaData {
+      public string info;
+      public string symbol;
+      public string last_ref;
+      public string interval;
+      public string output_size;
+      public string time_zone;
+
+      public static MetaData FromJSON(string json) {
+        return JsonUtility.FromJson<MetaData>(json);
+      }
+
+      public string toString() { // Debugging
+        StringBuilder dataBuilder = new StringBuilder();
+        dataBuilder.Append("info: ");
+        dataBuilder.AppendLine(info);
+        return dataBuilder.ToString();
+      }
     }
 
-    // All stock prices within a set amount of time
     [System.Serializable]
-    class AllStock {
-      public List<CompanyStock> allStock;
+    public class AllStock {
+      public MetaData metadata; 
+
+      // IMPORTANT: We can't create a list of stock data at each time interval
+      // because the data returned has timestamps as keys (meaning that all the keys
+      // are different and we can't serach by keys) 
+      public string data_intervals; 
 
       public static AllStock FromJSON(string json) {
-          return JsonUtility.FromJson<AllStock>(json);
-      } 
+        // NOTE must modify if we change the time series
+        // Removing whitespace between keys
+        json = json.Replace("Meta Data", "metadata").Replace("Time Series (5min)", "data_intervals");
+
+        // Yeah, this is ugly but IDK a better way to do it and it works SO DON'T TOUCH IT
+        // Metadata parsing
+        AllStock allStock = new AllStock();
+
+        json = json.Replace("1. Information", "info").Replace("2. Symbol", "symbol").
+                    Replace("3. Last Refreshed", "last_ref").Replace("4. Interval", "interval").
+                    Replace("5. Output Size", "output_size").Replace("6. Time Zone", "time_zone"); 
+        allStock.metadata = MetaData.FromJSON(JsonHelper.GetJsonObject(json, "metadata"));
+        Debug.Log("PARSED METADATA: " + allStock.metadata.toString());
+
+        // Breaking JSON string into different portions to make parsing work 
+        allStock.data_intervals = JsonHelper.GetJsonObject(json, "data_intervals");
+        return allStock;
+      }
+
+      public string toString() { // Debugging
+        StringBuilder dataBuilder = new StringBuilder();
+        dataBuilder.Append("metadata: ");
+        dataBuilder.AppendLine(metadata.toString());
+        dataBuilder.Append("data_intervals: ");
+        dataBuilder.AppendLine("[" + data_intervals + "]");
+        return dataBuilder.ToString();
+      }
     } 
 
-    // [System.Serializable]
-    class CompanyData {
+    public class CompanyData {
       public AllNews allNews;
       public AllStock allStock; 
+
+      // NOTE using news_name (doesn't rlly matter whether we use stock_name or news_name)
+      public string name;
+
+      public int xpos; // From PeriodTableJSON.json -- value from 1 to 6 inclusive
+      public static int xPosCurrVal = 1; // 1 - 6 inclusive
+      public static int MAX_XPOS = 6;
+
+      // Need to compute ypos using the number of companies we're displaying (I think) 
+      public int ypos;
+      public static int yPosCurrVal = 1; // 1 - x inclusive
+      public static int yPosCounter = 0; // # of instances of CompanyData with yPos == yPosCurrVal
+
+      public CompanyData(string name, string newsJson, string stockJson, int numCompanies) {
+        this.name = name;
+
+        allNews = AllNews.FromJSON(newsJson);
+        allStock = AllStock.FromJSON(stockJson);
+        
+        xpos = xPosCurrVal;
+        xPosCurrVal++;
+        if (xPosCurrVal == MAX_XPOS + 1) { 
+          xPosCurrVal = 1; 
+        }
+
+        // Compute the # of ypos positions we need 
+        // Keep yPos until we reach ceil(numCompanies / MAX_XPOS) instances of 
+        // CompanyData with yPos == yPosCurrVal
+        if (yPosCounter == Math.Ceiling((double)(numCompanies / MAX_XPOS))){
+          yPosCurrVal++;
+          yPosCounter = 1;
+        } else {
+          yPosCounter++;
+        }
+        ypos = yPosCurrVal;
+      }
+
+      public string toString() { // Debugging
+        return "news: " + allNews.toString() + "\nstock: " + allStock.toString();
+      }
     }
 
     [System.Serializable]
@@ -112,10 +216,9 @@ namespace HoloToolkit.MRDL.PeriodicTable
         }
     }
 
-public class Response
-{
-    public string result = "";
-}
+    public class Response {
+        public string result = "";
+    }
 
     public class PeriodicTableLoader : MonoBehaviour
     {
@@ -169,12 +272,13 @@ public class Response
         }
 
         private string GetDataFromAPI(string url) {
+          Debug.Log("URL: " + url);
           Response result = new Response();
           IEnumerator e = SendParallelApiRequest(result, url);
 
           // blocks here until UnityWebRequest() completes
-          while (e.MoveNext()) Debug.Log("WAITING");
-          Debug.Log("completed");
+          while (e.MoveNext());
+          Debug.Log("Finished");
           Debug.Log(result.result);
           return result.result;
         }
@@ -188,19 +292,23 @@ public class Response
             string NEWS_API_KEY = "c334b33f1acf4ba99b89dcc0bf595dd0";
             string NEWS_DATA_URL_FRONT = "http://newsapi.org/v2/everything?q="; // Concat name
             string NEWS_DATA_URL_BACK = "&from=2020-03-29&sortBy=popularity&apiKey=" + NEWS_API_KEY;
+
+            string STOCK_API_KEY = "B8LDV7QLIQNLCG1Y";
             string STOCK_DATA_URL_FRONT = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="; // Concat name 
-            string STOCK_DATA_URL_BACK = "&interval=5min&apikey=demo";
+            string STOCK_DATA_URL_BACK = "&interval=5min&apikey=" + STOCK_API_KEY;
+
+            int NUM_NEWS_ARTICLES = 3; // Number of news articles to store 
 
             // Stores company data
             List <CompanyData> allCompanyData = new List<CompanyData>();
 
-            // Iterate through them and make 2 API calls
             foreach (CompanyName companyName in companyNames) {
-              string name = companyName.name;
-              Debug.Log(name); 
+              string newsData = GetDataFromAPI(NEWS_DATA_URL_FRONT + companyName.news_name + NEWS_DATA_URL_BACK);
+              string stockData = GetDataFromAPI(STOCK_DATA_URL_FRONT + companyName.stock_name + STOCK_DATA_URL_BACK);
 
-              string news_data = GetDataFromAPI(NEWS_DATA_URL_FRONT + name + NEWS_DATA_URL_BACK);
-              string stock_data = GetDataFromAPI(STOCK_DATA_URL_FRONT + name + STOCK_DATA_URL_BACK);
+              CompanyData companyData = new CompanyData(companyName.news_name, newsData, stockData, companyNames.Count);
+              Debug.Log(companyData.toString());
+              allCompanyData.Add(companyData);
             }
         }
 
@@ -229,7 +337,7 @@ public class Response
             { "lanthanide", MatLanthanide },
         };
 
-
+            // TODO change elements to CompanyData
             if (isFirstRun == true)
             {
                 // Insantiate the element prefabs in their correct locations and with correct text
